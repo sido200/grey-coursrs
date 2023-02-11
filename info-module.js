8d5a6bd012b7ecf1b03bed80f7c0df0ffc376ad2
saliiimm <salimghalem40@gmmail.com>
2023-02-11 21:58:25 +0100

    commit-slm2

--- info-module.js
+++ info-module.js
@@ -42,21 +42,18 @@ const renderInfos = async () => {
 
         <div class ="buttons-menu">
         <div class ="chapitres-menu">
-          <button>${info.chap1}</button>
-          <button>${info.chap2}</button>
-          <button>${info.chap3}</button>
-          </div>
-            <button>TD's</button>
-            <button>TP's</button>
-            <button>Examens</button>
-            <button>Tests</button>
+      <a href="#chap1"><button>${info.chap1}</button></a>
+      <a href="#chap2"><button>${info.chap2}</button></a>
+      <a href="#chap3"><button>${info.chap3}</button></a>
+      <a href="#chap4"><button>${info.chap4}</button></a>
+            <a href="#TD"><button>TD's</button></a>
          </div>
-
+</div>
       </div>
       
       <div class="general-infos">
 
-      <h1>${info.chap1}:</h1>
+      <h1 id="chap1">${info.chap1}:</h1>
      <div class="infos-carres">
 
      <div class="element">
@@ -151,7 +148,7 @@ const renderInfos = async () => {
     </div>
      </div>
 </div>
- <h1>${info.chap2}:</h1>
+ <h1 id="chap2">${info.chap2}:</h1>
     <div class="infos-carres">
 
      <div class="element">
@@ -236,7 +233,7 @@ const renderInfos = async () => {
   
      </div>
 
-     <h1>${info.chap3}:</h1>
+     <h1 id="chap3">${info.chap3}:</h1>
     <div class="infos-carres">
 
      <div class="element">
@@ -356,11 +353,212 @@ const renderInfos = async () => {
   
      </div>
 
+   <h1 id="chap4">${info.chap4}:</h1>
+    <div class="infos-carres">
+
+     <div class="element">
+    <div class="titre-element">
+   <div class="titre-element-p1"> <h2>Introduction</h2></div>
+    <div class="titre-element-p2"> <h2>1</h2></div>
+    </div>
+    <div class="texte-element">
+    <div class="p-element">
+    <p>${info.intro_chap4}</p>
+    </div>
+    <div class="img-element">
+    <img src="./assets/image/arbre-intro-2.png" alt="">
+    </div>
+    <p>${info.p_chap4}</p>
+    <div class="img-element">
+<img src="./assets/image/Declaration-arbre.png" alt="">
+    </div>
+    </div>
+     </div>
+
+       <div class="element">
+    <div class="titre-element">
+    <div class="titre-element-p1"> <h2>${info.sous_titre_chap4_1}</h2></div>
+    <div class="titre-element-p2"> <h2>2</h2></div>
+    </div>
+    <div class="texte-element">
+    <p>${info.p_st_chap4_1}</p>
+   <img src="./assets/image/Initialisation-arbre.png" alt="">
+    </div>
+     </div>
+
+     <div class="element">
+    <div class="titre-element">
+     <div class="titre-element-p1"> <h2>${info.sous_titre_chap4_2}</h2></div>
+    <div class="titre-element-p2"> <h2>3</h2></div>
+    </div>
+    <div class="texte-element">
+    <p>${info.p_st_chap4_2}</p>
+   <img src="./assets/image/insert-arbre.png" alt="">
+    </div>
+     </div>
+
+     <div class="element">
+    <div class="titre-element">
+     <div class="titre-element-p1"> <h2>${info.sous_titre_chap4_3}</h2></div>
+    <div class="titre-element-p2"> <h2>4</h2></div>
+    </div>
+    <div class="texte-element">
+    <p>${info.p_st_chap4_3}</p>
+<img src="./assets/image/Recherch.png" alt="">
+    </div>
+     </div>
+
+      <div class="element">
+    <div class="titre-element">
+     <div class="titre-element-p1"> <h2>${info.sous_titre_chap4_4}</h2></div>
+    <div class="titre-element-p2"> <h2>5</h2></div>
+    </div>
+    <div class="texte-element">
+    <p>${info.p_st_chap4_4}</p>
+    <img src="./assets/image/arbre-intro-1.jpg" alt="">
+    </div>
+     </div>
+
+       <div class="element">
+    <div class="titre-element">
+     <div class="titre-element-p1"> <h2>${info.sous_titre_chap4_5}</h2></div>
+    <div class="titre-element-p2"> <h2>6</h2></div>
+    </div>
+    <div class="texte-element">
+    <p>${info.p_st_chap4_5}</p>
+<img src="./assets/image/Affichage-arbre.png" alt="">
+
+    </div>
+     </div>
+
+        <div class="element">
+    <div class="titre-element">
+     <div class="titre-element-p1"> <h2>Remarque</h2></div>
+    <div class="titre-element-p2"> <h2>7</h2></div>
+    </div>
+    <div class="texte-element">
+    <p>${info.remarque_chap_5}</p>
+    </div>
+     </div>
+
+      
+
+</div>
+
+<h1 id="TD">Exercices</h1>
+   <div class="divs-pdf">
+   
+   
+             
+
+      <div class="doc">
+        <a
+          href="https://drive.google.com/drive/folders/1DxX6khEOMZDXFUndigPfIa7iH68_feUL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            viewBox="0 0 1024 1024"
+            class="icon"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#32729a"
+            stroke="#32729a"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M576 102.4H268.8c-14.08 0-25.6 11.52-25.6 25.6v742.4c0 14.08 11.52 25.6 25.6 25.6h512c14.08 0 25.6-11.52 25.6-25.6V332.8L576 102.4z"
+                fill="#ffff"
+              ></path>
+              <path
+                d="M780.8 908.8H268.8c-21.76 0-38.4-16.64-38.4-38.4V128c0-21.76 16.64-38.4 38.4-38.4h312.32L819.2 327.68V870.4c0 21.76-16.64 38.4-38.4 38.4zM268.8 115.2c-7.68 0-12.8 5.12-12.8 12.8v742.4c0 7.68 5.12 12.8 12.8 12.8h512c7.68 0 12.8-5.12 12.8-12.8V337.92L570.88 115.2H268.8z"
+                fill="#ffff32729a"
+              ></path>
+              <path
+                d="M576 307.2c0 14.08 11.52 25.6 25.6 25.6h204.8L576 102.4v204.8z"
+                fill="#ffff008181"
+              ></path>
+              <path
+                d="M806.4 345.6H601.6c-21.76 0-38.4-16.64-38.4-38.4V102.4c0-5.12 2.56-10.24 7.68-11.52 5.12-2.56 10.24-1.28 14.08 2.56l230.4 230.4c3.84 3.84 5.12 8.96 2.56 14.08-1.28 5.12-6.4 7.68-11.52 7.68zM588.8 133.12V307.2c0 7.68 5.12 12.8 12.8 12.8h174.08L588.8 133.12zM332.8 435.2h371.2v25.6H332.8zM332.8 524.8h371.2v25.6H332.8z"
+                fill="#ffff32729a"
+              ></path>
+              <path
+                d="M332.8 614.4h371.2v25.6H332.8z"
+                fill="#ffff32729a"
+              ></path>
+              <path
+                d="M332.8 716.8h371.2v25.6H332.8z"
+                fill="#ffff32729a"
+              ></path>
+            </g>
+          </svg>
+          <h4> tableaux/listes</h4>
+        </a>
+      </div>
+
+      <div class="doc">
+        <a
+          href="https://drive.google.com/drive/folders/1eqOpMLSCX0AfQxDSMgpQerD8ZDumJhpL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            viewBox="0 0 1024 1024"
+            class="icon"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#32729a"
+            stroke="#32729a"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M576 102.4H268.8c-14.08 0-25.6 11.52-25.6 25.6v742.4c0 14.08 11.52 25.6 25.6 25.6h512c14.08 0 25.6-11.52 25.6-25.6V332.8L576 102.4z"
+                fill="#ffff"
+              ></path>
+              <path
+                d="M780.8 908.8H268.8c-21.76 0-38.4-16.64-38.4-38.4V128c0-21.76 16.64-38.4 38.4-38.4h312.32L819.2 327.68V870.4c0 21.76-16.64 38.4-38.4 38.4zM268.8 115.2c-7.68 0-12.8 5.12-12.8 12.8v742.4c0 7.68 5.12 12.8 12.8 12.8h512c7.68 0 12.8-5.12 12.8-12.8V337.92L570.88 115.2H268.8z"
+                fill="#ffff32729a"
+              ></path>
+              <path
+                d="M576 307.2c0 14.08 11.52 25.6 25.6 25.6h204.8L576 102.4v204.8z"
+                fill="#ffff008181"
+              ></path>
+              <path
+                d="M806.4 345.6H601.6c-21.76 0-38.4-16.64-38.4-38.4V102.4c0-5.12 2.56-10.24 7.68-11.52 5.12-2.56 10.24-1.28 14.08 2.56l230.4 230.4c3.84 3.84 5.12 8.96 2.56 14.08-1.28 5.12-6.4 7.68-11.52 7.68zM588.8 133.12V307.2c0 7.68 5.12 12.8 12.8 12.8h174.08L588.8 133.12zM332.8 435.2h371.2v25.6H332.8zM332.8 524.8h371.2v25.6H332.8z"
+                fill="#ffff32729a"
+              ></path>
+              <path
+                d="M332.8 614.4h371.2v25.6H332.8z"
+                fill="#ffff32729a"
+              ></path>
+              <path
+                d="M332.8 716.8h371.2v25.6H332.8z"
+                fill="#ffff32729a"
+              ></path>
+            </g>
+          </svg>
+          <h4>Arbres</h4>
+        </a>
+      </div>
+   
+   </div>
      </div>
 
       </div>
 
-
+    
 
     </div>
 
